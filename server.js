f0abb708aa488849a5ca13efb209a01a2587db5b
Kelly Cho <wenhsiu55@gmail.com>
2019-10-21 23:18:23 +0800

    Setting up GET & POST.

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-// Setup empty JS object to act as endpoint for all routes
+// setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// require Express to run server and routes
 const express = require('express');
 const bodyParser = require('body-parser')
 
-// Start up an instance of app
+// start up an instance of app
 const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* middleware*/
+// here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// cors for cross origin allowance
 const cors = require('cors');
 app.use(cors()); 
 
-// Initialize the main project folder
+// initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+// setup Server
 const port = 8000;
 
-// Spin up the server
-
+// spin up the server 
 const server = app.listen(port, listening);
 
 function listening() {
     console.log(`running on localhost: ${port}`);
 }
 
+// setup GET route
+app.get('/', sendData);
 
+function sendData(req, res) {
+	res.send('hiyooo');
+	return projectData;
+}
+
+// setup POST route
+app.post('/', postData);
+
+function postData(req, res) {
+	console.log(req.body);
+
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,47 @@
 /* Global Variables */
+let baseUrl = 'api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = '&APPID=0e2ec7dc3d32e820f8abdd569b0742b9';
 
-// Create a new date instance dynamically with JS
+// create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+
+
+/* async GET function */
+const getData = async (url = '') => {
+	const response = await fetch(url);
+
+	try {
+		const allData = await response.json();
+		console.log(allData);
+	} catch(error) {
+		console.log('error', error);
+	}
+}
+
+/* async POST function */
+const postData = async (url = '', data = {}) => {
+	console.log(data);
+
+	const response = await fetch (url, {
+	method: 'POST',	// * GET, POST, PUT, DELETE, etc.
+	credentials: 'same-origin', // include, *same-origin, omit
+	headers: {
+		'Content-Type': 'application/json', // indicates that our app run on JSON data
+	},
+	body: JSON.stringify(data), // body data type must match "Content-Type" header
+	});
+
+	try {
+		const newData = await response.json();
+		console.log(newData);
+		return newData;
+
+	} catch(error) {
+		console.log('error', error);
+	}
+}
+
+postData('/',{animal:'dogs'});
+getData(baseUrl + '92126' + apiKey);
